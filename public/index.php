bbfba234b8bbc8ea37f6483acf77b877e8fb7325
Yuan Chiu <chyuaner@gmail.com>
2025-07-23 15:45:20 +0800

    簡單做一下 index

--- public/index.php
+++ public/index.php
@@ -1,11 +1,30 @@
 <?php
 require __DIR__ . '/../bootstrap.php';
 
-use McModUtils\Mods;
+// use McModUtils\Mods;
 
 
-// echo Mods::modsPath();
-echo '<pre>';print_r(Mods::mods());echo '</pre>';
+// // echo Mods::modsPath();
+// echo '<pre>';print_r(Mods::mods());echo '</pre>';
 
-// $all_files = glob('{.[!.],}*', GLOB_BRACE);
-// echo '<pre>';print_r($all_files);echo '</pre>';
+// // $all_files = glob('{.[!.],}*', GLOB_BRACE);
+// // echo '<pre>';print_r($all_files);echo '</pre>';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minecraft 橋接遊戲伺服器用的後端</title>
+</head>
+<body>
+    <h1>Minecraft 橋接遊戲伺服器用的後端</h1>
+    <h2>已有功能</h2>
+    <ul>
+        <li>GET /mods</li>
+        <li>GET /mods/ApothicAttributes-1.21.1-2.9.0.jar</li>
+        <li>GET /files/mods/ApothicAttributes-1.21.1-2.9.0.jar</li>
+    </ul>
+</body>
+</html>
