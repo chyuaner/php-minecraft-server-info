bfe95548b68fb3db7e1cb28e2b692482c74eacfc
Yuan Chiu <chyuaner@gmail.com>
2025-07-29 15:03:05 +0800

    feat: index.php 處理預設頁面導向

--- public/index.php
+++ public/index.php
@@ -1,19 +1,26 @@
 <?php
 require __DIR__ . '/../bootstrap.php';
 
+$defaultApiFile = __DIR__.'/mods/index.php';
+
+// 如果有包含 text/html，就當作瀏覽器
+$type = 'json';
+if (!empty($_REQUEST['type']) && $_REQUEST['type'] == 'html' || str_contains($_SERVER['HTTP_ACCEPT'] ?? '', 'text/html')) {
+    $type = 'html';
+}
+if (!empty($_REQUEST['type']) && $_REQUEST['type'] == 'json') {
+    $type = 'json';
+}
+
+if ($type == 'json') {
+    include $defaultApiFile;
+    exit;
+}
+
 /**
  * @apiDefine McServers
  * @apiParam {String="youer1","youer2"} [server] 選填，伺服器名稱，例如 `youer1`。未填則使用預設伺服器。
  */
-
-// use McModUtils\Mods;
-
-
-// // echo Mods::modsPath();
-// echo '<pre>';print_r(Mods::mods());echo '</pre>';
-
-// // $all_files = glob('{.[!.],}*', GLOB_BRACE);
-// // echo '<pre>';print_r($all_files);echo '</pre>';
 ?>
 
 <!DOCTYPE html>
@@ -22,6 +29,12 @@ require __DIR__ . '/../bootstrap.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Minecraft 橋接遊戲伺服器用的後端</title>
+
+    <?php
+        if ($type=='html' && file_exists('docs/index.html')) {
+            echo '<meta http-equiv="refresh" content="0;url=/docs/index.html">';
+        }
+    ?>
 </head>
 <body>
     <h1>Minecraft 橋接遊戲伺服器用的後端</h1>
