294a9954a96fbe92c5508134dc6770122f27f476
Yuan Chiu <chyuaner@gmail.com>
2025-09-15 21:34:51 +0200

    fix public/mods empty type

--- public/mods/index.php
+++ public/mods/index.php
@@ -74,11 +74,11 @@ $type = 'json';
 $modFileName = null;
 
 // 如果有包含 text/html，就當作瀏覽器
-if ($_REQUEST['type'] == 'html' || str_contains($_SERVER['HTTP_ACCEPT'] ?? '', 'text/html')) {
+if (!empty($_REQUEST['type']) && $_REQUEST['type'] == 'html' || str_contains($_SERVER['HTTP_ACCEPT'] ?? '', 'text/html')) {
     $type = 'html';
     $enableCache = false; // 快取只針對JSON使用，所以非JSON就直接關閉快取
 }
-if ($_REQUEST['type'] == 'json' || str_contains($_SERVER['HTTP_ACCEPT'] ?? '', 'application/json')) {
+if (!empty($_REQUEST['type']) && $_REQUEST['type'] == 'json' || str_contains($_SERVER['HTTP_ACCEPT'] ?? '', 'application/json')) {
     $type = 'json';
     $enableCache = true;
 }
