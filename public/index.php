d6d4f793cf485413de5e5bfda0180d16befb45b2
Yuan Chiu <chyuaner@gmail.com>
2025-11-03 15:06:26 +0800

    fix apidoc

--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * @apiDefine McServers
+ * @apiParam {String="youer1","youer2"} [server] 選填，伺服器名稱，例如 `youer1`。未填則使用預設伺服器。
+ */
+
 use App\ResponseFormatter;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
