452a92ad386249236d46bc70d4ea54448fb8ea05
Yuan Chiu <chyuaner@gmail.com>
2025-11-04 19:57:19 +0800

    清除多餘的程式碼

--- src/McModUtils/Mods.php
+++ src/McModUtils/Mods.php
@@ -8,37 +8,14 @@ use ZipArchive;
 
 final class Mods
 {
-    private const ZIP_PATH = BASE_PATH.'/public/static/mods.zip';
-
-    public static function modsPath() : string {
-        return $GLOBALS['config']['mods_path'];
-    }
-
     // 這個模組資料夾的資訊
     private $path;
-    private $zip_path;
     private $ignorePrefixs = ['hide_'];
     private $onlyPrefixs = null;
     private $hashed;
 
     // 每個模組的陣列
     private $modsPathNameMap = [];
-
-    // public function doModNames() : array {
-    //     // 從設定檔取得mods資料夾路徑
-    //     $directory = self::modsPath();
-
-    //     // scandir寫法：取得所有mods檔名
-    //     // $files = scandir($directory);
-    //     // $files = array_diff(scandir($directory), array('..', '.'));
-
-    //     // glob寫法：取得所有mods檔名
-    //     $filePaths = glob($directory . '/*.jar');;
-    //     $files = array_map('basename', $filePaths);
-
-    //     $this->modNames = $files;
-    //     return $files;
-    // }
 
     public function __construct() {
         if (!empty($GLOBALS['config']['mods_path'])) {
@@ -178,68 +155,6 @@ final class Mods
     public static function mods() : array {
         $obj = new self();
         return $obj->getModNames();
-    }
-
-    public static function isFileExist($fileName) : bool {
-        $directory = self::modsPath();
-        $modFilePath = join(DIRECTORY_SEPARATOR, [rtrim($directory, '/'), $fileName]);
-        return file_exists($modFilePath);
-    }
-
-    public static function parseFileInput(string $raw) : string {
-        // TODO: 如果是帶入完整路徑
-        if (false) {
-            return $raw;
-        }
-        // 如果只帶檔案名稱
-        else {
-            return join(DIRECTORY_SEPARATOR, [rtrim(self::modsPath(), '/'), $raw]);
-        }
-    }
-
-    public static function zipPath() : string {
-        return self::ZIP_PATH;
-    }
-
-    public function getZipComment(string $zipPath = self::ZIP_PATH): ?string {
-        if (!file_exists($zipPath)) return null;
-        $zip = new ZipArchive();
-        if ($zip->open($zipPath) === true) {
-            $comment = $zip->getArchiveComment();
-            $zip->close();
-            return $comment;
-        }
-        return null;
-    }
-
-    public function zipFolder(string|null $source = null, string $zipPath = self::ZIP_PATH, string|null $comment = null): bool {
-        if (empty($source)) { $source = $this->getModsPath(); }
-        if (empty($comment)) { $comment = $this->getHashed(); }
-
-        $zip = new ZipArchive();
-        if (!$zip->open($zipPath, ZipArchive::CREATE | ZipArchive::OVERWRITE)) {
-            return false;
-        }
-
-        $zip->setArchiveComment($comment);
-
-        $source = realpath($source);
-        $sourceLen = strlen($source) + 1;
-
-        $files = $this->getModPaths();
-
-        foreach ($files as $filePath) {
-            $file = new SplFileInfo($filePath);
-            $relativePath = substr($filePath, $sourceLen);
-
-            if ($file->isDir()) {
-                $zip->addEmptyDir($relativePath);
-            } elseif ($file->isFile()) {
-                $zip->addFile($filePath, $relativePath);
-            }
-        }
-
-        return $zip->close();
     }
 
 }
